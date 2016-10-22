83d172504fbde68e663c44b4cc62f9c6a147ee0b
Nilton Vasques <niltonvasques@users.noreply.github.com>
2016-10-22 03:50:01 -0300

    Fix comments in unlock emulator script

--- unlock_emulator.sh
+++ unlock_emulator.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-// Only works for emulators > Lollipop
+# Only works for emulators > Lollipop
 if [ $(adb shell dumpsys input_method | grep mInteractive=false | wc -l) -eq 1 ]
 then 
   adb shell input keyevent 82
